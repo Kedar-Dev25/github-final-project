cd235010781f1300a6bf38bd541ad8fa21667d55
Kedar-Dev25 <kedar.code7@gmail.com>
2026-02-14 13:49:46 +0530

    Add DoctorService

---
+++ backend/src/main/java/com/smartclinic/service/DoctorService.java
@@ -0,0 +1,36 @@
+package com.smartclinic.service;
+
+import com.smartclinic.model.Doctor;
+import com.smartclinic.repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    // Get all doctors
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    // Get doctor by ID
+    public Optional<Doctor> getDoctorById(Long id) {
+        return doctorRepository.findById(id);
+    }
+
+    // Save new doctor
+    public Doctor saveDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    // Delete doctor by ID
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
+}
